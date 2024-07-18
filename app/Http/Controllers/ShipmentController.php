74ce03a2043227d33b8a7d695e5963ee8b198727
guvanchxh <maladisatabay356@gmail.com>
2024-07-18 17:42:59 +0500

    0.0.7

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Shipment;
 use Illuminate\Http\Request;
 
 class ShipmentController extends Controller
 {
-    //
+    public function index()
+    {
+        $shipments = Shipment::all();
+        return response()->json($shipments);
+    }
+
+    public function show($id)
+    {
+        $shipment = Shipment::findOrFail($id);
+        return response()->json($shipment);
+    }
+
+    public function store(Request $request)
+    {
+        $shipment = Shipment::create($request->all());
+        return response()->json($shipment, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $shipment = Shipment::findOrFail($id);
+        $shipment->update($request->all());
+        return response()->json($shipment, 200);
+    }
+
+    public function destroy($id)
+    {
+        Shipment::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
 }
