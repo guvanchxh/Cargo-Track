ae53d05327ecc0086994855d320db5ce6ba530d1
guvanchxh <maladisatabay356@gmail.com>
2024-07-18 17:45:32 +0500

    0.0.8

--- app/Http/Controllers/TransportMethodController.php
+++ app/Http/Controllers/TransportMethodController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransportMethod;
 use Illuminate\Http\Request;
 
 class TransportMethodController extends Controller
 {
-    //
+    public function index()
+    {
+        $transportMethods = TransportMethod::all();
+        return response()->json($transportMethods);
+    }
+
+    public function show($id)
+    {
+        $transportMethod = TransportMethod::findOrFail($id);
+        return response()->json($transportMethod);
+    }
+
+    public function store(Request $request)
+    {
+        $transportMethod = TransportMethod::create($request->all());
+        return response()->json($transportMethod, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $transportMethod = TransportMethod::findOrFail($id);
+        $transportMethod->update($request->all());
+        return response()->json($transportMethod, 200);
+    }
+
+    public function destroy($id)
+    {
+        TransportMethod::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
 }
