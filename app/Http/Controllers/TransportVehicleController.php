dc4ac39a87c12b8c0d34fbd8eb54a9f72e1717d9
guvanchxh <maladisatabay356@gmail.com>
2024-07-18 17:42:00 +0500

    0.0.6

--- app/Http/Controllers/TransportVehicleController.php
+++ app/Http/Controllers/TransportVehicleController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransportVehicle;
 use Illuminate\Http\Request;
 
 class TransportVehicleController extends Controller
 {
-    //
+    public function index()
+    {
+        $transportVehicles = TransportVehicle::all();
+        return response()->json($transportVehicles);
+    }
+
+    public function show($id)
+    {
+        $transportVehicle = TransportVehicle::findOrFail($id);
+        return response()->json($transportVehicle);
+    }
+
+    public function store(Request $request)
+    {
+        $transportVehicle = TransportVehicle::create($request->all());
+        return response()->json($transportVehicle, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $transportVehicle = TransportVehicle::findOrFail($id);
+        $transportVehicle->update($request->all());
+        return response()->json($transportVehicle, 200);
+    }
+
+    public function destroy($id)
+    {
+        TransportVehicle::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
 }
